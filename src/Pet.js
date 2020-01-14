7543e208c84b26940587e75065a592dc6ec27762
JPeoples5 <48669345+JPeoples5@users.noreply.github.com>
2020-01-13 21:46:46 -0600

    Add files via upload

---
+++ src/Pet.js
@@ -0,0 +1,21 @@
+import React from "react";
+//replacing {props} with the key is called restructuring
+//reduced the need to say props.name
+
+export default function Pet({ name, animal, breed }) {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+}
+/* JSX in action below
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+}
+*/
