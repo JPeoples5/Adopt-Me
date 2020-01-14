0f1d2e7893e3a7e900dc6b836401099687bd9bfc
JPeoples5 <48669345+JPeoples5@users.noreply.github.com>
2020-01-13 22:36:32 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 //pull just 'render' module from react-dom
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
+const x = "poo";
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Rudolph" animal="Rednose" breed="Reindeer" />
-      <Pet name="Iqarus" animal="Guinea Pig" breed="Slickback" />
+      <SearchParams />
     </div>
   );
 };
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//Parcel reaches out to FEM and installs {ANIMALS}
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  //this is a hook, all begin with 'use'| useState always give back an array
+  //never use hooks inside loops
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            //single line arrow functions {} are optional
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {/* no curly of arrowFunc means theres an implicit return */}
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
-return (
-  <div className="search-params">
-    <form>
-      <label htmlFor="location">
-        Location
-        <input id="location" value={location} placeholder="Location"></input>
-      </label>
-      <button>Submit</button>
-    </form>
-  </div>
-);
+export default SearchParams;
